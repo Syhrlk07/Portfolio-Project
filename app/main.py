3584a2cdfc329218b67e40287626e6e7077ea528
Syahrial Riefqi Nur Fauzi <syahrialriefqi20@gmail.com>
2026-08-03 00:03:54 +0700

    creating new output text for listing experts

--- app/main.py
+++ app/main.py
@@ -2,21 +2,32 @@ print("===============================")
 print("       Kiki Research AI")
 print("===============================")
 print("Knowledge Base is starting...")
-project_name = "Kiki Research AI"
+project_name = "=== Kiki Research AI ==="
 
 print(project_name)
 
+markdowns = [
+    "2026-06-19.md",
+    "2026-06-19(2).md",
+    "2026-07-01.md",
+    "2026-07-02.md",
+    "2026-07-03.md",
+]
+
 experts = [
-    "Adam Hart",
+    "Adam Hart (Grizzle)",
     "Amanda Natividad",
     "Anthony Pierri",
     "Brendan Hufford",
     "Chris Walker",
+    "Craig Taylor",
     "Devin Reed",
     "Rand Fishkin",
     "Ross Simmonds",
     "Samu Kovacs" 
 ]
 
+print(f"\nFound {len(experts)} experts:\n")
+
 for expert in experts:
-    print(expert)
+    print(f" - {expert}")
