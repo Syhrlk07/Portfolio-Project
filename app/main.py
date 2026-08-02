e1cf2e78bd4557e047c956c7d1b1f1225b9c3952
Syahrial Riefqi Nur Fauzi <syahrialriefqi20@gmail.com>
2026-08-03 01:19:24 +0700

    tell python to read research folder

--- app/main.py
+++ app/main.py
@@ -6,28 +6,14 @@ project_name = "=== Kiki Research AI ==="
 
 print(project_name)
 
-markdowns = [
-    "2026-06-19.md",
-    "2026-06-19(2).md",
-    "2026-07-01.md",
-    "2026-07-02.md",
-    "2026-07-03.md",
-]
 
-experts = [
-    "Adam Hart (Grizzle)",
-    "Amanda Natividad",
-    "Anthony Pierri",
-    "Brendan Hufford",
-    "Chris Walker",
-    "Craig Taylor",
-    "Devin Reed",
-    "Rand Fishkin",
-    "Ross Simmonds",
-    "Samu Kovacs" 
-]
+import os
 
-print(f"\nFound {len(experts)} experts:\n")
+folder = "research"
 
-for expert in experts:
-    print(f" - {expert}")
+folders = os.listdir(folder)
+
+print("Folders found:\n")
+
+for folder in folders:
+    print("-", folder)
