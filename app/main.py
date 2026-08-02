6c1c82d1c3238abb5e16fb964a28499de737e86d
Syahrial Riefqi Nur Fauzi <syahrialriefqi20@gmail.com>
2026-08-02 23:43:52 +0700

    Listing Experts

--- app/main.py
+++ app/main.py
@@ -2,3 +2,21 @@ print("===============================")
 print("       Kiki Research AI")
 print("===============================")
 print("Knowledge Base is starting...")
+project_name = "Kiki Research AI"
+
+print(project_name)
+
+experts = [
+    "Adam Hart",
+    "Amanda Natividad",
+    "Anthony Pierri",
+    "Brendan Hufford",
+    "Chris Walker",
+    "Devin Reed",
+    "Rand Fishkin",
+    "Ross Simmonds",
+    "Samu Kovacs" 
+]
+
+for expert in experts:
+    print(expert)
