f6a95afb9ae1792842ef3054597febf05b9e65b3
Syahrial Riefqi Nur Fauzi <syahrialriefqi20@gmail.com>
2026-08-08 00:43:21 +0700

    Refactor main.py to modularize functions and improve readability

--- app/main.py
+++ app/main.py
@@ -1,19 +1,52 @@
-print("===============================")
-print("       Kiki Research AI")
-print("===============================")
-print("Knowledge Base is starting...")
-project_name = "=== Kiki Research AI ==="
-
-print(project_name)
-
-
 import os
 
-folder = "research"
 
-folders = os.listdir(folder)
+def print_header():
+    print("=" * 40)
+    print("        KIKI RESEARCH AI")
+    print("=" * 40)
+    print()
 
-print("Folders found:\n")
 
-for folder in folders:
-    print("-", folder)
+def load_collections():
+    folder = "research"
+    return os.listdir(folder)
+
+
+def show_collections(collections):
+    print("Knowledge Base Loaded Successfully!\n")
+
+    print("Repository : Portfolio Project\n")
+
+    print("Collections Found:\n")
+
+    for index, collection in enumerate(collections, start=1):
+        print(f"{index}. {collection}")
+
+        print()
+
+
+def main():
+
+    print_header()
+
+    collections = load_collections()
+
+    show_collections(collections)
+
+    choice = input("Choose a collection: ")
+
+    if choice == "1":
+        print("\nOpening linkedin-posts...")
+
+    elif choice == "2":
+        print("\nOpening other...")
+
+    elif choice == "3":
+        print("\nOpening sources...")
+
+    elif choice == "4":
+        print("\nOpening youtube-transcripts...")
+
+
+main()
